1ec28dc691cd211963368b96c3eaff7e217b35a0
sluongo718 <sluongo718@yahoo.com>
2021-09-03 00:04:22 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    state = {
+        people: ''
+    }
+
+    render(){
+        return null
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(response => response.json())
+        .then(astros => this.setState({people: astros}))
+    }
+}
+
+
+// componentDidMount() {
+//     fetch("http://api.open-notify.org/astros.json")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.setState({
+//           peopleInSpace: data.people,
+//         });
+//       });
+//   }
+
